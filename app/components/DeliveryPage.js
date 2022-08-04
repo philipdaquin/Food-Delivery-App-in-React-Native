2586a95d183c5c09cd2ef98735d91bff8079ec2d
philipdaquin <philipdaquin20@gmail.com>
2022-08-04 08:16:12 +0800

    FINSIHED THIS MUFFAKER

--- app/components/DeliveryPage.js
+++ app/components/DeliveryPage.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { select_restaurant } from '../features/restaurant_slice';
 import {XIcon} from 'react-native-heroicons/solid'
 import * as Progress from 'react-native-progress'
+import MapView, {Marker}  from 'react-native-maps'
 
 
 const DeliveryPage = () => {
@@ -40,7 +41,44 @@ const DeliveryPage = () => {
             </View>
         </SafeAreaView>
         {/* Map */}
-        
+        <MapView initialRegion={{
+            // latitude: restaurant.lat, 
+            // longtitude: restaurant.long, 
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+        }} 
+            className="flex-1 -mt-10 z-0"
+            mapType="mutedStandard"
+        >
+            <Marker
+                coordinate={{
+                    latitude: restaurant.lat,
+                    longitude: restaurant.long,
+                }}  
+                title={restaurant.title}
+                description={restaurant.short_description}
+                identifier={"origin"}
+                pinColor="#00CCBB"    
+            />
+        </MapView>
+
+        <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28 ">
+            <Image
+                source={{
+                    uri: "https://links.papareact.com/wru"
+                }}
+                className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+            />
+            <View className="flex-1">
+                <Text className="text-gray-400">Person</Text>
+                <Text className="text-gray-400">Your Rider</Text>
+                
+            </View>
+                <Text className="text-[#00CCBB] text-lg mr-5 font-medium">Call</Text>
+
+        </SafeAreaView>
     </View>
   )
 }
